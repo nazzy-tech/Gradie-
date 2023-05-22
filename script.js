6a3ed635222b3c83370929c9a5626003859eb26e
nazzy-tech <58662791+nazzy-tech@users.noreply.github.com>
2023-05-22 15:11:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,43 @@
-// Email and Password Validation Function
-function validation(){
+function validate(){
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    var emailReg = /^([w-.]+@([w-]+.)+[w-] {2,4})?$/;
-    if( email === "" || password === ""){
-        alert("Please fill all fields!!!");
-        return false;
-    }
-    else if (!(email).match(emailReg)){
-        alert("Invalid Email!!!");
+
+    if(email == "admin@gmail.com" && password == "user"){
+        alert("Login Successful!!!");
         return false;
     }
     else{
-        return true;
+        alert("Login Failed!!!");
     }
 }
 
-// Submit form with id function
 
-function submit_by_id(){
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    if (validation()){ //Calling validation function
-        document.getElementById("formInput").submit; //Form submission
-        alert(" Email: " + email + " n Password : " + password + " n FormInput : " + document.getElementById("formInput").getAttribute("id") + "nn Form Submitted Successfully......");
-    } 
-}
+
+// Email and Password Validation Function
+// function validation(){
+//     var email = document.getElementById("email").value;
+//     var password = document.getElementById("password").value;
+//     var emailReg = /^([w-.]+@([w-]+.)+[w-] {2,4})?$/;
+//     if( email == "" || password == ""){
+//         alert("Please fill all fields!!!");
+//         return false;
+//     }
+//     else if (!(email).match(emailReg)){
+//         alert("Invalid Email!!!");
+//         return false;
+//     }
+//     else{
+//         return true;
+//     }
+// }
+
+// // Submit form with id function
+
+// function submit_by_id(){
+//     var email = document.getElementById("email").value;
+//     var password = document.getElementById("password").value;
+//     if (validation()){ //Calling validation function
+//         document.getElementById("formInput").submit; //Form submission
+//         alert(" Email: " + email + " n Password : " + password + " n FormInput : " + document.getElementById("formInput").getAttribute("id") + "nn Form Submitted Successfully......");
+//     } 
+// }
