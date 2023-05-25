15986bc528f5b452eb3d2aa6b21cb31cc548e486
nazzy-tech <58662791+nazzy-tech@users.noreply.github.com>
2023-05-25 13:37:22 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,70 @@
-function validate(){
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-
-    if(email == "admin@gmail.com" && password == "user"){
-        alert("Login Successful!!!");
-        return false;
-    }
-    else{
-        alert("Login Failed!!!");
-    }
-}
-
-
-
-// Email and Password Validation Function
-// function validation(){
-//     var email = document.getElementById("email").value;
-//     var password = document.getElementById("password").value;
-//     var emailReg = /^([w-.]+@([w-]+.)+[w-] {2,4})?$/;
-//     if( email == "" || password == ""){
-//         alert("Please fill all fields!!!");
-//         return false;
-//     }
-//     else if (!(email).match(emailReg)){
-//         alert("Invalid Email!!!");
-//         return false;
-//     }
-//     else{
-//         return true;
-//     }
+// function validate(){
+    // var email = document.getElementById("email").value;
+    // var password = document.getElementById("password").value;
+// 
+    // if(email == "" && password == ""){
+        // alert("Login Successful!!!");
+        // return false;
+    // }
+    // else{
+        // alert("Login Failed!!!");
+    // }
 // }
 
-// // Submit form with id function
+
+
+
+// Submit a form with javascript
+let formInput = document.getElementById("formInput");
+formInput.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    //handle submit
+
+    let myEmail = document.getElementById("email");
+    let myPassword = document.getElementById("password");
+
+    if (myEmail.value == "" || myPassword.value == ""){
+        alert("Ensure you input a value in this field!");
+    }else{
+        alert("This form has been successfully submitted!!!");
+        console.log(`Your email address ${myEmail.value} and password ${myPassword.value} have been submitted successfully`);
+
+        myEmail.value = "";
+        myPassword = "";
+    }
+});
+
+
+
+
+
+// 
+// Email and Password Validation Function
+// function validation(){
+    // let email = document.getElementById("email").value;
+    // let password = document.getElementById("password").value;
+    // let emailReg = /^([w-.]+@([w-]+.)+[w-] {2,4})?$/;
+    // if( email == "" || password == ""){
+        // alert("Please fill all fields!!!");
+        // return false;
+    // }
+    // else if (!(email).match(emailReg)){
+        // alert("Invalid Email!!!");
+        // return false;
+    // }
+    // else{
+        // return true;
+    // }
+// }
+// 
+// Submit form with id function
 
 // function submit_by_id(){
-//     var email = document.getElementById("email").value;
-//     var password = document.getElementById("password").value;
-//     if (validation()){ //Calling validation function
-//         document.getElementById("formInput").submit; //Form submission
-//         alert(" Email: " + email + " n Password : " + password + " n FormInput : " + document.getElementById("formInput").getAttribute("id") + "nn Form Submitted Successfully......");
-//     } 
+    // let email = document.getElementById("email").value;
+    // let password = document.getElementById("password").value;
+    // if (validation()){ //Calling validation function
+        // document.getElementById("formInput").submit; //Form submission
+        // alert(" Email: " + email + " n Password : " + password + " n FormInput : " + document.getElementById("formInput").getAttribute("id") + "nn Form Submitted Successfully......");
+    // } 
 // }
